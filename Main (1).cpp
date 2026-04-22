68fb51bb57626d35c9f9c8928a42d382824122c1
Khan <amberkhalil222@gmail.com>
2026-04-21 20:29:37 -0700

    INITIAL_CODE

---
+++ Main (1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "SCM Phases:\n";
+    cout << "1. Configuration Identification\n";
+    cout << "2. Configuration Control\n";
+    cout << "3. Status Accounting\n";
+    cout << "4. Verification & Audit\n";
+    cout << "5. Release Management\n";
+
+    return 0;
+}
